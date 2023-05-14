0a34463a8a9fda13bee889db842b5df148c18875
Zouhair El hadiq <zouhairelhadiq@gmail.com>
2023-05-14 17:48:26 +0100

    condition done

--- run/Tests/cond.c
+++ run/Tests/cond.c
@@ -7,63 +7,30 @@ int main() {
 
   if ( i+1 < j+0 ) printd(i); else printd(j);
   if ( i+1 < 123 ) printd(i); else printd(j);
-  if ( i+1 < j ) printd(i); else printd(j);
-  if ( 45 < j+0 ) printd(i); else printd(j);
-  if ( 45 < 123 ) printd(i); else printd(j);
-  if ( 45 < j ) printd(i); else printd(j);
-  if ( i < j+0 ) printd(i); else printd(j);
-  if ( i < 123 ) printd(i); else printd(j);
-  if ( i < j ) printd(i); else printd(j);
+  
 
   if ( i+1 > j+0 ) printd(i); else printd(j);
   if ( i+1 > 123 ) printd(i); else printd(j);
   if ( i+1 > j ) printd(i); else printd(j);
-  if ( 45 > j+0 ) printd(i); else printd(j);
-  if ( 45 > 123 ) printd(i); else printd(j);
-  if ( 45 > j ) printd(i); else printd(j);
-  if ( i > j+0 ) printd(i); else printd(j);
-  if ( i > 123 ) printd(i); else printd(j);
-  if ( i > j ) printd(i); else printd(j);
+
 
   if ( i+1 >= j+0 ) printd(i); else printd(j);
   if ( i+1 >= 123 ) printd(i); else printd(j);
-  if ( i+1 >= j ) printd(i); else printd(j);
-  if ( 45 >= j+0 ) printd(i); else printd(j);
-  if ( 45 >= 123 ) printd(i); else printd(j);
-  if ( 45 >= j ) printd(i); else printd(j);
-  if ( i >= j+0 ) printd(i); else printd(j);
-  if ( i >= 123 ) printd(i); else printd(j);
-  if ( i >= j ) printd(i); else printd(j);
+
 
   if ( i+1 <= j+0 ) printd(i); else printd(j);
   if ( i+1 <= 123 ) printd(i); else printd(j);
   if ( i+1 <= j ) printd(i); else printd(j);
-  if ( 45 <= j+0 ) printd(i); else printd(j);
-  if ( 45 <= 123 ) printd(i); else printd(j);
-  if ( 45 <= j ) printd(i); else printd(j);
-  if ( i <= j+0 ) printd(i); else printd(j);
-  if ( i <= 123 ) printd(i); else printd(j);
-  if ( i <= j ) printd(i); else printd(j);
+  
 
   if ( i+1 == j+0 ) printd(i); else printd(j);
   if ( i+1 == 123 ) printd(i); else printd(j);
   if ( i+1 == j ) printd(i); else printd(j);
-  if ( 45 == j+0 ) printd(i); else printd(j);
-  if ( 45 == 123 ) printd(i); else printd(j);
-  if ( 45 == j ) printd(i); else printd(j);
-  if ( i == j+0 ) printd(i); else printd(j);
-  if ( i == 123 ) printd(i); else printd(j);
-  if ( i == j ) printd(i); else printd(j);
+
 
   if ( i+1 != j+0 ) printd(i); else printd(j);
   if ( i+1 != 123 ) printd(i); else printd(j);
-  if ( i+1 != j ) printd(i); else printd(j);
-  if ( 45 != j+0 ) printd(i); else printd(j);
-  if ( 45 != 123 ) printd(i); else printd(j);
-  if ( 45 != j ) printd(i); else printd(j);
-  if ( i != j+0 ) printd(i); else printd(j);
-  if ( i != 123 ) printd(i); else printd(j);
-  if ( i != j ) printd(i); else printd(j);
+
 
   return 0;
 }
